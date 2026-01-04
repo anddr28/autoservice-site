0782a342987ca52cf2ba153f5e75f665c4564b57
Andrew <dronan2008@gmail.com>
2026-01-05 02:22:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function submitForm(e) {
     'Заявка отправлена. Мы свяжемся с вами.';
   return false;
 }
+
+const btn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  btn.classList.toggle('show', window.scrollY > 300);
+});
+
+btn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
